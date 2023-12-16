3dee09f848a4bac26aeafbd72c5dba6451a4fc70
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2023-12-16 08:39:50 -0300

    Fix for inherited external class, with full path of the class.

--- packages/pastojs/src/fppas2js.pp
+++ packages/pastojs/src/fppas2js.pp
@@ -16192,6 +16192,14 @@ var
       end;
   end;
 
+  function GetFullClassPath(const ClassType: TPasClassType): String;
+  begin
+    Result := ClassType.ExternalName;
+
+    if ClassType.Parent is TPasClassType then
+      Result := GetFullClassPath(TPasClassType(ClassType.Parent)) + '.' + Result;
+  end;
+
 var
   aResolver: TPas2JSResolver;
   DelaySrc: TJSSourceElements;
@@ -16305,7 +16313,7 @@ begin
     if Ancestor=nil then
       AncestorPath:='null'
     else if AncestorIsExternal then
-      AncestorPath:=TPasClassType(Ancestor).ExternalName
+      AncestorPath:=GetFullClassPath(TPasClassType(Ancestor))
     else
       AncestorPath:=CreateReferencePath(Ancestor,AContext,rpkPathAndName);
     Call.AddArg(CreatePrimitiveDotExpr(AncestorPath,El));
