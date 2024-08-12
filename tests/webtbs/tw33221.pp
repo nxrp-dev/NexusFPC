a09d0972c9f6a75d9417bc1784f59c7b60047b63
Rika Ichinose <rrunewalsh@gmail.com>
2024-08-12 22:51:28 +0300

    Allow DuplicateLocals for objects (unless in Delphi mode).

--- tests/webtbs/tw33221.pp
+++ tests/webtbs/tw33221.pp
@@ -2,7 +2,8 @@ program Project1;
 
 {$mode objfpc}{$H+}
 {$ModeSwitch duplicatelocals}
-
+{$ModeSwitch advancedrecords}
+ 
 type
   TClass1 = class(TObject)
   public
@@ -18,8 +19,45 @@ begin
 end;
 
 // Local variable has same name as member field.
-// Unlike with delphi mode, this is compiler error, even with duplicatelocals!
+// This is okay with duplicatelocals, too.
 procedure TClass1.Proc2;
+var
+  X: Integer;
+begin
+end;
+
+// Same for objects and managed records. (Though in Delphi mode, which implicitly enables duplicatelocals, disallowed specifically for objects.)
+type
+  TRecord1 = record
+  public
+    X: Integer;
+    procedure Proc1(X: Integer);
+    procedure Proc2;
+  end;
+
+procedure TRecord1.Proc1(X: Integer);
+begin
+end;
+
+procedure TRecord1.Proc2;
+var
+  X: Integer;
+begin
+end;
+
+type
+  TObject1 = object
+  public
+    X: Integer;
+    procedure Proc1(X: Integer);
+    procedure Proc2;
+  end;
+
+procedure TObject1.Proc1(X: Integer);
+begin
+end;
+
+procedure TObject1.Proc2;
 var
   X: Integer;
 begin
