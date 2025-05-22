b3a1bf95c658d76462773d2490da627253e5bac4
J. Gareth "Curious Kit" Moreton <gareth@moreton-family.com>
2025-05-22 01:42:21 +0100

    * Added basic type conversion for MM lanes

--- compiler/ncgcnv.pas
+++ compiler/ncgcnv.pas
@@ -531,6 +531,25 @@ interface
                     internalerror(2003012261);
                 end;
               end;
+            LOC_MMLANE,
+            LOC_CMMLANE:
+              begin
+                case expectloc of
+                  LOC_FPUREGISTER:
+                    begin
+                      hlcg.location_force_fpureg(current_asmdata.CurrAsmList,left.location,left.resultdef,false);
+                      location.register:=cg.getfpuregister(current_asmdata.CurrAsmList,location.size);
+                      hlcg.a_loadfpu_reg_reg(current_asmdata.CurrAsmList,left.resultdef,resultdef,left.location.register,location.register);
+                    end;
+                  LOC_MMREGISTER:
+                    begin
+                      location.register:=hlcg.getmmregister(current_asmdata.CurrAsmList,resultdef);
+                      hlcg.a_loadmm_lane_reg(current_asmdata.CurrAsmList,left.resultdef,resultdef,left.location.mmlane,location.register,mms_movescalar);
+                    end;
+                  else
+                    internalerror(2003012261);
+                end;
+              end;
             else
               internalerror(2002032215);
          end;
