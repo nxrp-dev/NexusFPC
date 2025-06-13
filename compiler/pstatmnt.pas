1f031ea60b194244204f6bd9cf2e703679e51779
Frederic Kehrein <frederic@kehrein.org>
2025-06-14 00:00:21 +0200

    Adding StatementExpressions Modeswitch

--- compiler/pstatmnt.pas
+++ compiler/pstatmnt.pas
@@ -1866,6 +1866,8 @@ implementation
 
     function statement_expr(var p1 : tnode) : boolean;
       begin
+        if not (m_statement_expressions in current_settings.modeswitches) then
+          exit(false);
         result:=true;
         case token of
         _IF: p1:=if_statement(true);
