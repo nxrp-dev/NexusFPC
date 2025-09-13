e53a86f9330d32c5568ff589a85908374e8cf6e2
J. Gareth "Kit" Moreton <gareth@moreton-family.com>
2025-09-13 22:17:35 +0100

    * x86: "AddMov2LeaAdd" and "SubMov2LeaSub" now raise internal errors if the          registers are the same in the MOV instructions (should never happen)

--- compiler/x86/aoptx86.pas
+++ compiler/x86/aoptx86.pas
@@ -16570,14 +16570,14 @@ unit aoptx86;
                     { Change the MOV instruction to a LEA instruction, and update the
                       first operand }
 
+                    if MatchOperand(taicpu(hp1).oper[0]^,taicpu(hp1).oper[1]^.reg) then
+                      { All "mov %reg,%reg" instructions should have been removed }
+                      InternalError(2025091301);
+
                     reference_reset(NewRef, 1, []);
                     NewRef.base := taicpu(p).oper[1]^.reg;
                     NewRef.scalefactor := 1;
-                    { if the destination reg is the same as the ADD register,
-                      and we keep the ADD instruction, do not add the offset
-                      to LEA instruction, otherwise the reg gets increased by 2 times the offset value }
-                    if DoAddMov2Lea or not MatchOperand(taicpu(hp1).oper[0]^,taicpu(hp1).oper[1]^.reg) then
-                      NewRef.offset := asizeint(taicpu(p).oper[0]^.val);
+                    NewRef.offset := asizeint(taicpu(p).oper[0]^.val);
 
                     taicpu(hp1).opcode := A_LEA;
                     taicpu(hp1).loadref(0, NewRef);
@@ -16850,14 +16850,15 @@ unit aoptx86;
                   begin
                     { Change the MOV instruction to a LEA instruction, and update the
                       first operand }
+
+                    if MatchOperand(taicpu(hp1).oper[0]^,taicpu(hp1).oper[1]^.reg) then
+                      { All "mov %reg,%reg" instructions should have been removed }
+                      InternalError(2025091302);
+
                     reference_reset(NewRef, 1, []);
                     NewRef.base := taicpu(p).oper[1]^.reg;
                     NewRef.scalefactor := 1;
-                    { if the destination reg is the same as the SUB register,
-                      and we keep the ADD instruction, do not substract the offset
-                      to LEA instruction, otherwise the reg gets decreased by 2 times the offset value }
-                    if DoSubMov2Lea or not MatchOperand(taicpu(hp1).oper[0]^,taicpu(hp1).oper[1]^.reg) then
-                       NewRef.offset := -taicpu(p).oper[0]^.val;
+                    NewRef.offset := -taicpu(p).oper[0]^.val;
 
                     taicpu(hp1).opcode := A_LEA;
                     taicpu(hp1).loadref(0, NewRef);
