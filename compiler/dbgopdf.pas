49f4a7932de0d0b70b4cc66c9c8b27818b89ff88
Graeme Geldenhuys <graemeg@gmail.com>
2026-02-16 19:11:52 +0000

    Phase 1E: Implement working OPDF type and symbol emission
    The OPDF writer now produces valid .opdf files with ordinal type
    definitions and global variable symbols. Previously inserttypeinfo
    and insertmoduleinfo were stubs.
    
    Key changes to dbgopdf.pas:
    - inserttypeinfo: Initialize defnumberlist/deftowritelist (required
      by inherited write_symtable_* methods), create output stream,
      iterate symbol tables, and clean up dbg_state after writing
    - insertmoduleinfo: Finalize OPDF output
    - appenddef_ord: Write primitive type records with name, size, signedness
    - appendsym_staticvar: Write global variable records with type ID
    - SetOutputStream: Map FPC target_cpu to OPDF architecture enum
    - Add required imports: classes, sysutils, fmodule, globtype
    - Remove debug WriteLn statements
    
    Tested: ppcx64 -gO produces a valid .opdf file with correct type
    and variable records for test_01_primitives.pas.
    
    Note: Line info (insertlineinfo) not yet implemented - requires
    architectural decision on embedded vs external OPDF storage.

--- compiler/dbgopdf.pas
+++ compiler/dbgopdf.pas
@@ -27,10 +27,13 @@ unit dbgopdf;
 interface
 
 uses
-  cclasses,
+  cclasses, classes,
+  sysutils,
   aasmtai, aasmdata,
   systems,
   symbase,symconst,symtype,symdef,symsym,
+  fmodule,
+  globtype,
   DbgBase,
   tpdf_type_mapper,
   ogopdf, opdf_io, opdf_demangle;
@@ -43,7 +46,15 @@ type
     FTypeMapper: TTypeMapper;
     { OPDF writer for binary output }
     FWriter: TOPDFWriter;
+    { Output stream for OPDF file }
+    FStream: TStream;
   protected
+    { Mark types used by all variables in a symbol table }
+    procedure MarkVariableTypesUsed(st: TSymtable);
+
+    { Initialize output stream }
+    procedure SetOutputStream(AStream: TStream);
+
     { Override TDebugInfo virtual methods for type definitions }
     procedure appenddef_ord(list: TAsmList; def: TOrdDef); override;
     procedure appenddef_float(list: TAsmList; def: TFloatDef); override;
@@ -93,6 +104,7 @@ begin
   inherited Create;
   FTypeMapper := TTypeMapper.Create;
   FWriter := nil; { Will be created when output file is opened }
+  FStream := nil;
 end;
 
 destructor TOPDFDebugWriter.Destroy;
@@ -100,168 +112,321 @@ begin
   if assigned(FWriter) then
     FWriter.Free;
   FTypeMapper.Free;
+  { Note: FStream is owned by caller, don't free here }
   inherited Destroy;
 end;
 
-{ Type definition handlers - Phase 1D stubs }
+procedure TOPDFDebugWriter.MarkVariableTypesUsed(st: TSymtable);
+var
+  i: Longint;
+  def: TDef;
+begin
+  if not assigned(st) or not assigned(st.DefList) then
+    Exit;
+
+  { For OPDF Phase 1E, mark all types in DefList as used so they get collected }
+  { This is appropriate for collecting debug information }
+  for i := 0 to st.DefList.Count - 1 do
+  begin
+    def := TDef(st.DefList[i]);
+    if assigned(def) and (def.dbg_state = dbg_state_unused) then
+      def.dbg_state := dbg_state_used;
+  end;
+end;
+
+procedure TOPDFDebugWriter.SetOutputStream(AStream: TStream);
+var
+  Arch: TTargetArch;
+  PointerSize: Byte;
+begin
+  FStream := AStream;
+
+  { Determine target architecture }
+  if target_cpu = cpu_x86_64 then
+    Arch := archX86_64
+  else if target_cpu = cpu_i386 then
+    Arch := archI386
+  else if target_cpu = cpu_arm then
+    Arch := archARM
+  else if target_cpu = cpu_aarch64 then
+    Arch := archAArch64
+  else
+    Arch := archUnknown;
+
+  { Determine pointer size based on target }
+  PointerSize := 8; { Default to 64-bit }
+  if target_cpu = cpu_i386 then
+    PointerSize := 4
+  else if target_cpu = cpu_arm then
+    PointerSize := 4;
+
+  { Create the OPDF writer }
+  FWriter := TOPDFWriter.Create(FStream, Arch, PointerSize);
+
+  { Write the header }
+  if assigned(FWriter) then
+    FWriter.WriteHeader;
+end;
+
+{ Type definition handlers - Phase 1E+ implementation }
 
 procedure TOPDFDebugWriter.appenddef_ord(list: TAsmList; def: TOrdDef);
+var
+  TypeID: Cardinal;
+  TypeName: String;
+  Size: Integer;
+  IsSigned: Boolean;
 begin
-  { Phase 1E: Implement ordinal type mapping }
-  if assigned(def) then
-    FTypeMapper.GetTypeID(def);
+  if not assigned(def) or not assigned(FWriter) then
+    Exit;
+
+  { Get or allocate TypeID }
+  TypeID := FTypeMapper.GetTypeID(def);
+
+  { Determine type name }
+  TypeName := def.GetTypeName;
+
+  { Get size in bytes }
+  Size := def.size;
+  if Size < 1 then
+    Size := 1; { Fallback for unknown sizes }
+
+  { Determine signedness based on ordtype }
+  IsSigned := False;
+  case def.ordtype of
+    s8bit, s16bit, s32bit, s64bit, s128bit: IsSigned := True;
+    else IsSigned := False;
+  end;
+
+  { Write primitive type to OPDF }
+  FWriter.WritePrimitive(TypeID, TypeName, Size, IsSigned);
 end;
 
 procedure TOPDFDebugWriter.appenddef_float(list: TAsmList; def: TFloatDef);
 begin
-  { Phase 1E: Implement float type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_enum(list: TAsmList; def: TEnumDef);
 begin
-  { Phase 1E: Implement enumeration type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_array(list: TAsmList; def: TArrayDef);
 begin
-  { Phase 2C: Implement array type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_record(list: TAsmList; def: TRecordDef);
 begin
-  { Phase 3A: Implement record type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_object(list: TAsmList; def: TObjectDef);
 begin
-  { Phase 3B: Implement class type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_pointer(list: TAsmList; def: TPointerDef);
 begin
-  { Phase 2B: Implement pointer type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_string(list: TAsmList; def: TStringDef);
 begin
-  { Phase 2A: Implement string type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_procvar(list: TAsmList; def: TProcVarDef);
 begin
-  { Phase 4A: Implement procedural type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_variant(list: TAsmList; def: TVariantDef);
 begin
-  { Phase 2A: Implement variant type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_set(list: TAsmList; def: TSetDef);
 begin
-  { Phase 2A: Implement set type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_file(list: TAsmList; def: TFileDef);
 begin
-  { Phase 2A: Implement file type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_formal(list: TAsmList; def: TFormalDef);
 begin
-  { Phase 2A: Implement formal type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
 procedure TOPDFDebugWriter.appenddef_undefined(list: TAsmList; def: TUndefinedDef);
 begin
-  { Phase 2A: Implement undefined type mapping }
   if assigned(def) then
     FTypeMapper.GetTypeID(def);
 end;
 
-{ Symbol handlers - Phase 1D stubs }
+{ Symbol handlers }
 
 procedure TOPDFDebugWriter.appendsym_staticvar(list: TAsmList; sym: TStaticVarSym);
+var
+  TypeID: Cardinal;
+  VarName: String;
+  Address: QWord;
 begin
-  { Phase 4A: Implement static variable mapping }
+  if not assigned(sym) or not assigned(FWriter) or not assigned(sym.vardef) then
+    Exit;
+
+  { Skip external variables (their addresses are resolved at link time) }
+  if vo_is_external in sym.varoptions then
+    Exit;
+
+  { Get the variable's type ID }
+  TypeID := FTypeMapper.GetTypeID(sym.vardef);
+
+  { Get the variable name }
+  VarName := sym.name;
+
+  { Address 0 - the debugger resolves real addresses using symbol names }
+  Address := 0;
+
+  { Write the global variable to OPDF }
+  FWriter.WriteGlobalVar(VarName, TypeID, Address);
 end;
 
 procedure TOPDFDebugWriter.appendsym_paravar(list: TAsmList; sym: TParaVarSym);
 begin
-  { Phase 4A: Implement parameter variable mapping }
+  { TODO: Implement parameter variable mapping }
 end;
 
 procedure TOPDFDebugWriter.appendsym_localvar(list: TAsmList; sym: TLocalVarSym);
 begin
-  { Phase 4A: Implement local variable mapping }
+  { TODO: Implement local variable mapping }
 end;
 
 procedure TOPDFDebugWriter.appendsym_fieldvar(list: TAsmList; sym: TFieldVarSym);
 begin
-  { Phase 3A: Implement field variable mapping }
+  { TODO: Implement field variable mapping }
 end;
 
 procedure TOPDFDebugWriter.appendsym_const(list: TAsmList; sym: TConstSym);
 begin
-  { Phase 2A: Implement constant symbol mapping }
+  { TODO: Implement constant symbol mapping }
 end;
 
 procedure TOPDFDebugWriter.appendsym_type(list: TAsmList; sym: TTypeSym);
 begin
-  { Phase 1D: Implement type symbol mapping }
+  { TODO: Implement type symbol mapping }
 end;
 
 procedure TOPDFDebugWriter.appendsym_label(list: TAsmList; sym: TLabelSym);
 begin
-  { Phase 4A: Implement label symbol mapping }
+  { TODO: Implement label symbol mapping }
 end;
 
 procedure TOPDFDebugWriter.appendsym_absolute(list: TAsmList; sym: TAbsoluteVarSym);
 begin
-  { Phase 2A: Implement absolute variable mapping }
+  { TODO: Implement absolute variable mapping }
 end;
 
 procedure TOPDFDebugWriter.appendsym_property(list: TAsmList; sym: TPropertySym);
 begin
-  { Phase 3C: Implement property mapping }
+  { TODO: Implement property mapping }
 end;
 
 { Main entry points }
 
 procedure TOPDFDebugWriter.inserttypeinfo;
+var
+  OPDFFilename: String;
+  DummyList: TAsmList;
+  i: longint;
+  def: tdef;
 begin
-  { Phase 1E: Write collected type information to OPDF }
-  { For now, this is a stub. Type info is collected during appenddef_* calls }
+  { Initialize the output stream BEFORE collecting types }
+  if FStream = nil then
+  begin
+    if assigned(current_module) then
+    begin
+      OPDFFilename := ChangeFileExt(current_module.objfilename, '.opdf');
+      try
+        FStream := TFileStream.Create(OPDFFilename, fmCreate);
+        SetOutputStream(FStream);
+      except
+        on E: Exception do
+        begin
+          WriteLn('Warning: Could not create OPDF file ', OPDFFilename);
+          WriteLn('Error: ', E.Message);
+          Exit;
+        end;
+      end;
+    end
+    else
+      Exit; { No module, can't proceed }
+  end;
+
+  { Initialize base class lists required by inherited write_symtable_* methods }
+  defnumberlist := TFPObjectList.Create(false);
+  deftowritelist := TFPObjectList.Create(false);
+
+  { Collect and write all types and symbols }
+  DummyList := TAsmList.create;
+  try
+    { Write types from used units }
+    write_used_unit_type_info(DummyList, current_module);
+
+    { Write types from global symbol table }
+    if assigned(current_module.globalsymtable) then
+      write_symtable_defs(DummyList, current_module.globalsymtable);
+
+    { Write types from local symbol table }
+    if assigned(current_module.localsymtable) then
+      write_symtable_defs(DummyList, current_module.localsymtable);
+
+    { Write symbols (variables) from global symbol table }
+    if assigned(current_module.globalsymtable) then
+      write_symtable_syms(DummyList, current_module.globalsymtable);
+
+    { Write symbols (variables) from local symbol table }
+    if assigned(current_module.localsymtable) then
+      write_symtable_syms(DummyList, current_module.localsymtable);
+  finally
+    DummyList.Free;
+  end;
+
+  { Reset dbg_state on all tracked defs so they can be reused }
+  for i := 0 to defnumberlist.count - 1 do
+  begin
+    def := tdef(defnumberlist[i]);
+    if assigned(def) then
+      def.dbg_state := dbg_state_unused;
+  end;
+
+  defnumberlist.free;
+  defnumberlist := nil;
+  deftowritelist.free;
+  deftowritelist := nil;
 end;
 
 procedure TOPDFDebugWriter.insertmoduleinfo;
 begin
-  { Phase 1E: Write module information and finalize OPDF }
-  { For now, this is a stub. Module info will be written when compilation completes }
+  { Finalize the OPDF output }
+  if assigned(FWriter) then
+    FWriter.Finalize;
 end;
 
 initialization
